4ba9303569fb4feab92ebdef7f94d369e2f41cd7
henriquearruda259 <84778136+henriquearruda259@users.noreply.github.com>
2021-05-28 09:14:35 -0300

    Create Celular.java

---
+++ Celular.java
@@ -0,0 +1,39 @@
+package Atividade;
+
+public class celular {
+
+	public celular(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public char[] getTipoAparelho() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void carregar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getMAH() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public char[] getModeloProcessador() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPotencia() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTipoTela() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
